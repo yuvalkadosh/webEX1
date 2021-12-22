b0eb2ddc689677bcc6f8fbb27286233f9f5caf97
Yuval Kadosh <yuvalkadosh5@gmail.com>
2021-12-22 11:45:42 +0200

    plans html page

--- webex1/webex1/validators.py
+++ webex1/webex1/validators.py
@@ -10,22 +10,17 @@ from django.contrib.auth.hashers import (
 class DictValidator(object):
     def __init__(self, dict_path=None):
         self.dict_path = dict_path
-
     def validate(self, password, user=None):
         isValid = True
         f = open(self.dict_path, "r")
-        for line in f:
-            
+        for line in f: 
             if password == line.strip():
                 isValid = False
-
-
         if not isValid:
             raise ValidationError(
                 _("Your password is predictable and published online, try a different one!"),
                 code='password_in_dict',
             )
-
     def get_help_text(self):
         return _(
             "Your password must not be predictable"
@@ -35,7 +30,6 @@ class DictValidator(object):
 class ComplexValidator(object):
     def __init__(self, categories_amount=3):
         self.categories_amount = categories_amount
-
     def validate(self, password, user=None):
         count = 0
         if re.findall('[a-z]', password):
@@ -52,7 +46,6 @@ class ComplexValidator(object):
                 _("Your password must contains " + str(self.categories_amount) + " of the 4 character categories: Lowercase, Uppercase, Numbers, Symbols"),
                 code='password_not_complex',
             )
-
     def get_help_text(self):
         return _(
             "Your password must contains " + str(self.categories_amount) + " of the 4 character categories: Lowercase, Uppercase, Numbers, Symbols"
@@ -62,12 +55,8 @@ class ComplexValidator(object):
 class HistoryValidator(object):
     def __init__(self, last_pass_amount=1):
        self.last_pass_amount = last_pass_amount
-
     def validate(self, password, user=None):
         isNotValid=False
-
-
-
         if self.last_pass_amount >= 1 and user.password != None:
             isNotValid = isNotValid or check_password(password,user.password)
         if self.last_pass_amount >= 2 and user.password2 != None:
@@ -82,7 +71,6 @@ class HistoryValidator(object):
                 _("The password must be different then last passwords!"),
                 code='password_not_different_then_last',
             )
-
     def get_help_text(self):
         return _(
             "The password must be different then last passwords!"
